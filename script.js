fca435aa6b44008b37ac3196e117b8ef30db5e04
GMiranda0432 <gbrldemiranda@gmail.com>
2026-06-08 08:06:19 -0300

    arquivos base

---
+++ script.js
@@ -0,0 +1,7 @@
+const btnPoupanca = document.getElementById("btnPoupanca");
+const btnTesouroDireto = document.getElementById("btnTesouroDireto");
+const btnAcoes = document.getElementById("btnAcoes");
+const btnCriptoativos = document.getElementById("btnCriptoativos");
+const btnCDB = document.getElementById("btnCDB");
+const btnLCI = document.getElementById("btnLCI");
+const btnLCA = document.getElementById("btnLCA");
